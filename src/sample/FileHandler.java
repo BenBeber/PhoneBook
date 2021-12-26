603115028838614c8c631ffb63ef2b938086bf84
BenBeber <63212349+BenBeber@users.noreply.github.com>
2021-12-26 20:10:05 +0200

    Update FileHandler.java
    list of things to do

--- src/sample/FileHandler.java
+++ src/sample/FileHandler.java
@@ -5,7 +5,10 @@ import javafx.stage.FileChooser;
 import java.io.File;
 
 public  class FileHandler {
-
+        //refctor name to choseFile
+        //add file var to the class
+        //add constructor to handle the file load
+        //add method to get file
     public static File getFile(ContactsList.FileMode mode) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Please select a file");
